19ff75b629edc830c1de15c1657b82e39428678d
Qendolin <32160662+Qendolin@users.noreply.github.com>
2024-03-26 21:06:53 +0100

    improve make script

--- make.bat
+++ make.bat
@@ -8,6 +8,8 @@ if "%~1"=="release" goto RELEASE
 if "%~1"=="install" goto INSTALL
 if "%~1"=="clean" goto CLEAN
 if "%~1"=="rebuild" goto REBUILD
+echo Invalid option "%~1"
+goto :DONE
 @ECHO ON
 
 :BLANK
@@ -36,8 +38,10 @@ rmdir /Q /S %INSTALL_DIR% 2>NUL
 GOTO DONE
 
 :REBUILD
-rmdir /Q /S "build/Debug/" 2>NUL
-rmdir /Q /S "build/PTVC_Project_GL.dir/" 2>NUL
+rmdir /Q /S "build\Debug\" 2>NUL
+rmdir /Q /S "build\PTVC_Project_GL.dir\" 2>NUL
+del /q "build\PTVC_Project_GL.*" 2>NUL
+cmake -H. -B %PROJECT_DIR% -A "x64" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR%
 cmake --build %PROJECT_DIR% --config Debug --target ALL_BUILD
 GOTO DONE
 
